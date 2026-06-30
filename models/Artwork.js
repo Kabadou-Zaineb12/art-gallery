f263bea9394aebd421e0246c17baeab897459964
zaineb58 <kabadou.zaineb@gmail.com>
2026-06-30 12:17:58 +0100

    feat: complete step 1 - project setup & artwork CRUD

---
+++ models/Artwork.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const artworkSchema = new mongoose.Schema({
+  title: String,
+  imageURL: String,
+  tags: [String],
+  startingPrice: Number,
+  ownerId: String,
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Artwork", artworkSchema);
